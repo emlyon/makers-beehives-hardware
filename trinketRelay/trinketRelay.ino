71b81d9f423f153d8d0700c1a5a8b17d1dca6832
Lionel RADISSON <makio135@gmail.com>
2018-09-28 13:49:49 +0200

    simpler alternative

--- trinketRelay/trinketRelay.ino
+++ trinketRelay/trinketRelay.ino
@@ -5,14 +5,12 @@ Trinket ----> Relay
      #4 ----> IN
 */
 void setup() {
-  pinMode( 1, OUTPUT );
-  digitalWrite( 1, HIGH );
   pinMode( 4, OUTPUT );
 }
 
 void loop() {
+  digitalWrite( 4, HIGH );
+  delay( 3300000 );
   digitalWrite( 4, LOW );
   delay( 300000 );
-  digitalWrite( 4, HIGH );
-  delay( 3600000 );
 }
